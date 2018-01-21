7ebb2e9fcf57a153e79902a5ec4fcaad6da2878e
gerardavecilla <gerardavecilla@ufl.edu>
2018-01-20 19:11:38 -0500

    Initial commit

---
+++ app/src/main/java/com/example/gerard/foodreminderapp/FoodItem.java
@@ -0,0 +1,11 @@
+package com.example.gerard.foodreminderapp;
+
+
+public class FoodItem {
+    private String foodName;
+    private int foodExpiry;
+    public FoodItem(String foodName, int foodExpiry){
+        this.foodName = foodName;
+        this.foodExpiry = foodExpiry;
+    }
+}
---
+++ app/src/main/java/com/example/gerard/foodreminderapp/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.gerard.foodreminderapp;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView mListView;
+    protected ArrayAdapter<FoodItem> listAdapter;
+    ArrayList<FoodItem> foodList = new ArrayList<>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //FoodItem banana = new FoodItem("banana", 111);
+        //foodList.add(banana);
+
+        
+        super.onCreate(savedInstanceState);
+        mListView = findViewById(R.id.list_view);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog(view);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    public void openDialog(View view){
+        Intent intent = new Intent(this, DialogActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
+
+
